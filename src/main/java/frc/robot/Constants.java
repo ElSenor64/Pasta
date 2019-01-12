a207ef6a3bbdb446bcf9ddbb028a37b2f97d870d
ElSenor64 <sillynoodle36@gmail.com>
2019-01-12 10:00:53 -0700

    Converting to controller class management.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,5 +2,6 @@ package frc.robot;
 
 public class Constants {
     static final int MAN_CON_PORT = 0;
-
+    static final int OPEN_CLAW_TRIGGER = 3;
+    static final int CLOSE_CLAW_TRIGGER = 2;
 }
--- src/main/java/frc/robot/Controllers.java
+++ src/main/java/frc/robot/Controllers.java
@@ -4,9 +4,20 @@ import edu.wpi.first.wpilibj.Joystick;
 
 public class Controllers {
     private Joystick manipulatorStick;
+    private double openClawTrigger;
+    private double closeClawTrigger;
 
     public Controllers() {
         manipulatorStick = new Joystick(Constants.MAN_CON_PORT);
         
+    }
+
+    public void setControllerValues() {
+        openClawTrigger = manipulatorStick.getRawAxis(Constants.OPEN_CLAW_TRIGGER);
+        closeClawTrigger = manipulatorStick.getRawAxis(Constants.CLOSE_CLAW_TRIGGER);
+    }
+
+    public double getCloseClawTrigger() {
+        return closeClawTrigger;
     }
 }
