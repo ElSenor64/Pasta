db04e369e18a5fe0acf4d1e8547cf2babf9bceb6
ElSenor64 <sillynoodle36@gmail.com>
2019-01-11 19:38:31 -0700

    More files made

---
+++ src/main/java/frc/robot/Controllers.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class Controllers {
+    private Joystick manipulatorStick;
+
+    public Controllers() {
+        manipulatorStick = new Joystick(0);
+        
+    }
+}
